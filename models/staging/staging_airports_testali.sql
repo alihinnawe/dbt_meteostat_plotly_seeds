efec5bad56712e453d7db33d6f92a04c0f23db5e
alihinnawe <hinnaweali@gmail.com>
2026-09-24 17:32:12 +0200

    rename sql for airports

--- models/staging/staging_airports_testali.sql
+++ models/staging/staging_airports_testali.sql
@@ -1,1 +1,7 @@
-SELECT * FROM {{source('flights_data', 'airports')}}
+WITH airports_regions_join1 AS (
+        SELECT * 
+        FROM {{source('flights_data', 'airports')}}
+        LEFT JOIN {{source('flights_data', 'regions')}}
+        USING (country)
+    )
+    SELECT * FROM airports_regions_join1
