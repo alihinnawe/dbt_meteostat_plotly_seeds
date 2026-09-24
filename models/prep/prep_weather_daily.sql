c371ac6902632cd215a4112da024b168fbb28bb1
alihinnawe <hinnaweali@gmail.com>
2026-09-24 14:55:19 +0200

    fixing the capetilization of month

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -1,27 +1,32 @@
-    WITH daily_data AS (
-        SELECT * 
-        FROM {{ref('staging_weather_daily')}}
-    ),
-    add_features AS (
-        SELECT *
-    		, DATE_PART('day', date) AS date_day 		-- number of the day of month
-    		, DATE_PART('month', date) AS date_month 	-- number of the month of year
-    		, DATE_PART('year', date) AS date_year 		-- number of year
-    		, DATE_PART('week', date) AS cw 			-- number of the week of year
-    		, TO_CHAR(date, 'FMmonth') AS month_name 	-- name of the month
-    		, TO_CHAR(date, 'FMday') AS weekday 		-- name of the weekday
-        FROM daily_data 
-    ),
-    add_more_features AS (
-        SELECT *
-    		, (CASE 
-    			WHEN month_name in ('December','January','February') THEN 'winter'
-    			WHEN month_name in ('March','April','May') THEN 'spring'
-                WHEN month_name in ('June','July','August') THEN 'summer'
-                WHEN month_name in ('September','October','November') THEN 'autumn'
-    		END) AS season
-        FROM add_features
-    )
+WITH daily_data AS (
     SELECT *
-    FROM add_more_features
-    ORDER BY date
+    FROM {{ ref('staging_weather_daily') }}
+),
+
+add_features AS (
+    SELECT
+        *,
+        DATE_PART('day', date) AS date_day,
+        DATE_PART('month', date) AS date_month,
+        DATE_PART('year', date) AS date_year,
+        DATE_PART('week', date) AS cw,
+        TO_CHAR(date, 'FMmonth') AS month_name,
+        TO_CHAR(date, 'FMday') AS weekday
+    FROM daily_data
+),
+
+add_more_features AS (
+    SELECT
+        *,
+        CASE
+            WHEN month_name IN ('december', 'january', 'february') THEN 'winter'
+            WHEN month_name IN ('march', 'april', 'may') THEN 'spring'
+            WHEN month_name IN ('june', 'july', 'august') THEN 'summer'
+            WHEN month_name IN ('september', 'october', 'november') THEN 'autumn'
+        END AS season
+    FROM add_features
+)
+
+SELECT *
+FROM add_more_features
+ORDER BY date
